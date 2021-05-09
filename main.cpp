55f31e3d59f7d1710f42afae40ef2af4a7bc3b43
BenG49 <ben@goldfisher.com>
2021-05-09 00:12:50 -0400

    Fix

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 
 using namespace sf;
 
-RenderWindow win(VideoMode(640, 480), ".", Style::Titlebar);
+RenderWindow win(VideoMode(600, 600), ".", Style::Titlebar);
 
 enum dir { UP, LEFT, DOWN, RIGHT };
 
@@ -13,14 +13,40 @@ struct vec2i
 {
     int x, y;
 
+    vec2i() : x(0), y(0) {};
+    vec2i(const int& x, const int& y) : x(x), y(y) {};
+    vec2i(const vec2i& other)
+    {
+        x = other.x;
+        y = other.y;
+    }
+
+    vec2i& operator = (const vec2i& other)
+    {
+        x = other.x;
+        y = other.y;
+        return *this;
+    }
+
+    bool operator == (const vec2i& vec) const
+    {
+        return x == vec.x && y == vec.y;
+    }
+
     bool within(vec2i a, vec2i size)
     {
         return x > a.x && x < a.x+size.x && y > a.y && y < a.y+size.y;
     }
+
+    std::size_t operator()(const vec2i& vec) const
+    {
+        std::size_t hash = 0x85ebca6b * x + 0xcc9e2d51 * y;
+        return hash;
+    }
 };
 
-std::unordered_set<vec2i> tiles;
-vec2i ant = { 0, 0 };
+std::unordered_set<vec2i, vec2i> tiles({ vec2i(5, 5) });
+vec2i ant(0, 0);
 int direction = UP;
 
 void update()
@@ -48,7 +74,7 @@ void update()
         ++ant.x;
 }
 
-void draw(vec2i center, int squareCountY)
+/*void draw(vec2i center, int squareCountY)
 {
     int squareSize = floor(480.0f/squareCountY);
 
@@ -65,8 +91,31 @@ void draw(vec2i center, int squareCountY)
     for (auto it = tiles.begin(); it != tiles.end(); ++it)
     {
         vec2i temp = { it->x, it->y };
+        // draw
         if (temp.within(topLeft, size))
         {
+            
+        }
+    }
+}*/
+
+void d()
+{
+    vec2i topleft = { -10, -10 };
+    vec2i size = { 20, 20 };
+    // 30
+    RectangleShape sq(Vector2f(30, 30));
+    sq.setFillColor(Color::Black);
+    sq.setOutlineThickness(0);
+
+    for (auto it = tiles.begin(); it != tiles.end(); ++it)
+    {
+        vec2i temp = { it->x, it->y };
+        // draw
+        if (temp.within(topleft, size))
+        {
+            sq.setPosition(Vector2f(topleft.x + temp.x * 30, topleft.y + temp.y * 30));
+            win.draw(sq);
         }
     }
 }
@@ -82,6 +131,7 @@ int main()
                 win.close();
 
         win.clear(Color::White);
+        d();
         win.display();
     }
 
