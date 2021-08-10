5a3ece60a96983dbbb6c8e7b48e96441655746ea
BenG49 <ben@goldfisher.com>
2021-08-09 23:52:45 -0400

    Redid code

--- main.cpp
+++ main.cpp
@@ -1,161 +1,161 @@
 #include <SFML/Graphics.hpp>
-#include <unordered_set>
+#include <vector>
 #include <iostream>
 #include <math.h>
 
-using namespace sf;
-
 const int milliDelta = 10;
 
-RenderWindow win(VideoMode(600, 600), "ant", Style::Titlebar);
-Clock clk;
-int milliUpdate = 100;
-bool running = false;
+enum Dir { UP, LEFT, DOWN, RIGHT };
 
-int viewSize = 21;
-
-enum dir { UP, LEFT, DOWN, RIGHT };
-
-struct vec2i
+struct Vec2i
 {
     int x, y;
 
-    vec2i() : x(0), y(0) {};
-    vec2i(const int& x, const int& y) : x(x), y(y) {};
-    vec2i(const vec2i& other)
-    {
-        x = other.x;
-        y = other.y;
-    }
+    Vec2i()
+        : x(0)
+        , y(0) {};
+    Vec2i(const int& x, const int& y)
+        : x(x)
+        , y(y) {};
+    Vec2i(const Vec2i& other)
+        : x(other.x)
+        , y(other.y) {}
 
-    bool operator == (const vec2i& vec) const
+    bool operator==(const Vec2i& vec) const
     {
         return x == vec.x && y == vec.y;
     }
 
-    vec2i operator + (const vec2i& vec)
+    Vec2i operator+(const Vec2i& vec)
     {
-        return vec2i(this->x + vec.x, this->y + vec.y);
+        return Vec2i(this->x + vec.x, this->y + vec.y);
     }
 
-    vec2i operator - (const vec2i& vec)
+    Vec2i operator-(const Vec2i& vec)
     {
-        return vec2i(this->x - vec.x, this->y - vec.y);
+        return Vec2i(this->x - vec.x, this->y - vec.y);
     }
 
-    vec2i operator * (const int& other)
+    Vec2i operator*(const int& other)
     {
-        return vec2i(this->x * other, this->y * other);
+        return Vec2i(this->x * other, this->y * other);
     }
 
-    vec2i operator / (const int& other)
+    Vec2i operator/(const int& other)
     {
-        return vec2i(this->x / other, this->y / other);
+        return Vec2i(this->x / other, this->y / other);
     }
 
-    bool within(vec2i a, vec2i size)
+    bool within(Vec2i a, Vec2i size)
     {
         return x > a.x && x < a.x+size.x && y > a.y && y < a.y+size.y;
     }
 
-    std::size_t operator()(const vec2i& vec) const
+    std::size_t operator()(const Vec2i& vec) const
     {
         std::size_t hash = 0x85ebca6b * x + 0xcc9e2d51 * y;
         return hash;
     }
 };
 
-std::unordered_set<vec2i, vec2i> tiles({});
-vec2i ant(0, 0);
-int direction = UP;
-
 void update()
 {
-    // black tile
-    if (tiles.find(ant) != tiles.end())
-    {
-        direction = (direction + 1) % 4;
-        tiles.erase(ant);
-    }
-    // white tile
-    else
-    {
-        direction = (direction == 0) ? 3 : (direction - 1);
-        tiles.insert(ant);
-    }
-
-    if (direction == UP)
-        ++ant.y;
-    else if (direction == LEFT)
-        --ant.x;
-    else if (direction == DOWN)
-        --ant.y;
-    else if (direction == RIGHT)
-        ++ant.x;
-}
-
-void draw()
-{
-    RectangleShape sq(Vector2f(1, 1));
-    sq.setFillColor(Color::Black);
-    sq.setOutlineThickness(0);
-
-    for (auto it = tiles.begin(); it != tiles.end(); ++it)
-    {
-        sq.setPosition(Vector2f(it->x, it->y));
-        win.draw(sq);
-    }
-
-    sq.setPosition(Vector2f(ant.x+0.125, ant.y+0.125));
-    sq.setFillColor(Color::Red);
-    sq.setSize(Vector2f(0.75, 0.75));
-
-    win.draw(sq);
 }
 
 int main()
 {
-    Event pollEvent;
-    View view(Vector2f(0, 0), Vector2f(viewSize, viewSize));
+    int viewSize = 21;
+    int milliUpdate = 100;
+    bool running = false;
+    sf::Clock clk;
+
+    sf::RenderWindow win(sf::VideoMode(600, 600), "ant", sf::Style::Titlebar);
+    sf::View view(sf::Vector2f(0.5f, 0.5f), sf::Vector2f(viewSize, viewSize));
 
     win.setView(view);
 
+    std::vector<Vec2i> tiles;
+    Vec2i ant(0, 0);
+    int direction = UP;
+
     while (win.isOpen())
     {
-        while (win.pollEvent(pollEvent))
+        sf::Event event;
+        while (win.pollEvent(event))
         {
-            if (pollEvent.type == Event::MouseWheelScrolled)
+            if (event.type == sf::Event::MouseWheelScrolled)
             {
-                viewSize = std::max(viewSize - pollEvent.mouseWheelScroll.delta, 11.0f);
-                if (viewSize != view.getSize().x)
-                {
-                    view.setSize(Vector2f(viewSize, viewSize));
-                    win.setView(view);
-                }
-            }
-            else if (pollEvent.type == Event::KeyPressed)
-            {
-                int code = pollEvent.key.code;
+                if (event.mouseWheelScroll.delta < 0)
+                    view.zoom(1.6f);
+                else
+                    view.zoom(0.625f);
 
-                if (code == Keyboard::Up)
+                win.setView(view);
+            }
+            else if (event.type == sf::Event::KeyPressed)
+            {
+                int code = event.key.code;
+
+                if (code == sf::Keyboard::Up)
                     milliUpdate = std::max(milliUpdate - milliDelta, 0);
-                if (code == Keyboard::Down)
+                if (code == sf::Keyboard::Down)
                     milliUpdate += milliDelta;
-                if (code == Keyboard::Space)
+                if (code == sf::Keyboard::Space)
                     running = !running;
             }
-            else if (pollEvent.type == Event::Closed)
+            else if (event.type == sf::Event::Closed)
                 win.close();
         }
 
-        win.clear(Color::White);
+        win.clear(sf::Color::White);
+
         if (running && clk.getElapsedTime().asMilliseconds() >= milliUpdate)
         {
-            update();
             clk.restart();
+
+            // ----- UPDATE GAME ----- //
+            // black tile
+            auto it_pos = std::find(tiles.begin(), tiles.end(), ant);
+            if (it_pos != tiles.end())
+            {
+                direction = (direction + 1) % 4;
+                tiles.erase(it_pos);
+            }
+            // white tile
+            else
+            {
+                direction = (direction == 0) ? 3 : (direction - 1);
+                tiles.push_back(ant);
+            }
+
+            if (direction == UP)
+                ++ant.y;
+            else if (direction == LEFT)
+                --ant.x;
+            else if (direction == DOWN)
+                --ant.y;
+            else if (direction == RIGHT)
+                ++ant.x;
+            // ----- ----------- ----- //
         }
 
-        draw();
+        // ----- DRAW SQUARES ----- //
+        sf::RectangleShape sq(sf::Vector2f(1, 1));
+        sq.setFillColor(sf::Color(7, 40, 100));
+
+        for (auto it = tiles.begin(); it != tiles.end(); ++it)
+        {
+            sq.setPosition(sf::Vector2f(it->x, it->y));
+            win.draw(sq);
+        }
+
+        sq.setPosition(sf::Vector2f(ant.x+0.125, ant.y+0.125));
+        sq.setFillColor(sf::Color(170, 0, 0));
+        sq.setSize(sf::Vector2f(0.75, 0.75));
+
+        win.draw(sq);
+        // ----- ------------ ----- //
+
         win.display();
     }
 
